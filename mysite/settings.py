693031c547b1b32301658f223329485d4ea7c772
DextoneNone <mr.banek@bk.ru>
2021-03-20 19:47:15 +0700

    asd

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'cud(9v1#+j$7&j2f2=)8w#y-25!j2dgk)5uq-19b%9wa9f*9uc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["IvanArsenev.pythonanywhere.com","127.0.0.1"]
 
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
